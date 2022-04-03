43183144e15ea0c9a41421f0349a1463bca0ab8c
Kjetil H <haugkjet@gmail.com>
2022-04-03 22:28:03 +0200

    Test deployment of small change

--- src/App.js
+++ src/App.js
@@ -5,20 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React xxx
-        </a>
         <h1 class="text-3xl text-green-500 font-bold" >Welcome</h1>
+        <body>
+      <div class="flex">
+         <h1 class="text-xl text-indigo-500 font-bold" >Test</h1>
+
+      </div>
+
+      </body>
       </header>
+
     </div>
   );
 }
