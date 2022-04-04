0e6c6de48a455bcbcb1aa3e2c00b44d468bc2e22
Kjetil H <haugkjet@gmail.com>
2022-04-04 22:59:47 +0200

    Changed class to classname

--- src/components/PriceTracker.js
+++ src/components/PriceTracker.js
@@ -1,14 +1,9 @@
 function PriceTracker (props) {
 
-    const ticker = 'BTC';
-    const price = '24526.4';
-
-    const lastDate = new Date (2022,4,4);
-
-    return <div class="flex">
-                <div class="text-blue-500 font-bold text-sm ml-4">{props.ticker}</div>
-                <div class="text-red-500 font-bold text-sm ml-4">{props.price}</div>
-                <div class="text-yellow-500 font-bold text-sm ml-4">{props.lastDate.toISOString()}</div>
+    return <div className="flex">
+                <div className="text-blue-500 font-bold text-sm ml-4">{props.ticker}</div>
+                <div className="text-red-500 font-bold text-sm ml-4">{props.price}</div>
+                <div className="text-yellow-500 font-bold text-sm ml-4">{props.lastUpdate.toISOString()}</div>
             </div>
 }
 
