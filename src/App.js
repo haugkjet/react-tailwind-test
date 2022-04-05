e310bbe38c81c9b22190977ad3d567cc362d3764
Kjetil H <haugkjet@gmail.com>
2022-04-05 10:09:35 +0200

    Added button with useState

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ function App() {
 
     <div className="App">
  
-        <h1 class="text-3xl text-green-500 font-bold" >Welcome</h1>
-        <body>
-      <PriceTracker ticker={pricesToTrack[0].ticker} price={pricesToTrack[0].price} lastDate={pricesToTrack[0].lastUpdate} ></PriceTracker>
-      <PriceTracker ticker={pricesToTrack[1].ticker} price={pricesToTrack[1].price} lastDate={pricesToTrack[1].lastUpdate}></PriceTracker>
-      <PriceTracker ticker={pricesToTrack[2].ticker} price={pricesToTrack[2].price} lastDate={pricesToTrack[2].lastUpdate}></PriceTracker>
-      <PriceTracker ticker={pricesToTrack[3].ticker} price={pricesToTrack[3].price} lastDate={pricesToTrack[3].lastUpdate}></PriceTracker>
+        <h1 className="text-3xl text-green-500 font-bold">Welcome</h1>
+      
+      <PriceTracker ticker={pricesToTrack[0].ticker} price={pricesToTrack[0].price} lastUpdate={pricesToTrack[0].lastUpdate} ></PriceTracker>
+      <PriceTracker ticker={pricesToTrack[1].ticker} price={pricesToTrack[1].price} lastUpdate={pricesToTrack[1].lastUpdate}></PriceTracker>
+      <PriceTracker ticker={pricesToTrack[2].ticker} price={pricesToTrack[2].price} lastUpdate={pricesToTrack[2].lastUpdate}></PriceTracker>
+      <PriceTracker ticker={pricesToTrack[3].ticker} price={pricesToTrack[3].price} lastUpdate={pricesToTrack[3].lastUpdate}></PriceTracker>
 
-      </body>
+     
 
     </div>
   );
--- src/components/PriceTracker.js
+++ src/components/PriceTracker.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 function PriceTracker (props) {
 
+    //let newTicker = props.ticker;
+
+    const [ticker, setTicker] = useState(props.ticker);
+
+    const clickHandler = ()  => {
+        setTicker("Updated");
+        console.log(ticker)
+    };
+
     return <div className="flex">
-                <div className="text-blue-500 font-bold text-sm ml-4">{props.ticker}</div>
+                <div className="text-blue-500 font-bold text-sm ml-4">{ticker}</div>
                 <div className="text-red-500 font-bold text-sm ml-4">{props.price}</div>
-                <div className="text-yellow-500 font-bold text-sm ml-4">{props.lastUpdate.toISOString()}</div>
+                <div className="text-yellow-500 font-bold text-sm ml-4 pr-4">{props.lastUpdate.toISOString()}</div>
+                <button onClick={clickHandler}className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1">Get price</button>
             </div>
 }
 
